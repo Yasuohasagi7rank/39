bcf177be0221d8318694bf78f8ef7ab6a1a84023
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:33:55 +0300

    Add files via upload

---
+++ 11.45/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _11._45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e;
+            a = int.Parse(Console.ReadLine());
+            b = a / 10;
+            c = a % 10;
+            d = Convert.ToInt32(Math.Pow(a, 2));
+            e = 4 * Convert.ToInt32(Math.Pow(b, 3)) + 4 * Convert.ToInt32(Math.Pow(c, 3));
+            if (d == e)
+                Console.WriteLine("Равен");
+            else
+                Console.WriteLine("Не равен");
+        }
+    }
+}
